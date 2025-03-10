bd23e225dc316f21c32ef874467f9d6fa7cacec8
Swapnil Pande <swapnil.pande98@gmail.com>
2025-03-10 14:55:28 -0400

    Full joystick data transmitting

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,29 @@
 #include <WiFi.h>
 #include <esp_now.h>
 
-typedef struct JoystickData
+struct JoystickData
 {
-  float x;
-  float y;
-} JoystickData;
+  bool a;
+  bool b;
+  bool x;
+  bool y;
+  bool lb;
+  bool rb;
+  bool dpad_up;
+  bool dpad_down;
+  bool dpad_left;
+  bool dpad_right;
+  bool start;
+  bool back;
+  float lt;
+  float rt;
+  float left_x;
+  float left_y;
+  float right_x;
+  float right_y;
+};
+
+JoystickData joystick;
 
 JoystickData receivedData;
 
@@ -15,10 +33,17 @@ void OnDataRecv(const esp_now_recv_info *info, const uint8_t *incomingData,
 {
   memcpy(&receivedData, incomingData, sizeof(receivedData));
 
-  Serial.print("X: ");
-  Serial.print(receivedData.x);
-  Serial.print("\tY: ");
-  Serial.println(receivedData.y);
+  //   Print all joystick data, 1 line
+  Serial.printf(
+      "A: %d, B: %d, X: %d, Y: %d, LB: %d, RB: %d, Dpad Up: %d, Dpad Down: %d, "
+      "Dpad Left: %d, Dpad Right: %d, Start: %d, Back: %d, LT: %f, RT: %f, "
+      "Left X: %f, Left Y: %f, Right X: %f, Right Y: %f\n",
+      receivedData.a, receivedData.b, receivedData.x, receivedData.y,
+      receivedData.lb, receivedData.rb, receivedData.dpad_up,
+      receivedData.dpad_down, receivedData.dpad_left, receivedData.dpad_right,
+      receivedData.start, receivedData.back, receivedData.lt, receivedData.rt,
+      receivedData.left_x, receivedData.left_y, receivedData.right_x,
+      receivedData.right_y);
 }
 
 void setup()
